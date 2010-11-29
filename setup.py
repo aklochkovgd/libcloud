4774e4e1d913deb459363c6e08a19fa3f958e8b2
Roman Bogorodskiy <bogorodskiy@gmail.com>
2010-11-29 17:11:29 +0300

    Bump again.

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ class ApiDocsCommand(Command):
 
 setup(
     name='apache-libcloud',
-    version='0.3.1.2',
+    version='0.3.1.3',
     description='A unified interface into many cloud server providers',
     author='Apache Software Foundation',
     author_email='libcloud@incubator.apache.org',
